6830ed8f9f7c83debdbb981d7277db2303619a1c
saul <2068363905@qq.com>
2023-01-14 15:27:22 +0800

    hot-fix commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gitj");
         System.out.println("hhhjjj");
+        System.out.println("hello hot");
 
     }
 }
